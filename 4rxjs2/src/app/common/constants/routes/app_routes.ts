e2e15a04d7259e41a394f7ed8157ad68ac01e54e
LenaLenina <lenalenina5677@gmail.com>
2020-02-02 20:52:42 +0300

    1

--- 4rxjs2/src/app/common/constants/routes/app_routes.ts
+++ 4rxjs2/src/app/common/constants/routes/app_routes.ts
@@ -3,9 +3,3 @@ export const APP_ROUTES = {
     rxjs2: { name: 'RXJS2', url: 'rxjs2' },
     rxjs3: { name: 'RXJS3', url: 'rxjs3' },
 }
-
-export const APP_ROUTES_TO_SIDEBAR = [
-    { name: APP_ROUTES.rxjs.name, url: '/' + APP_ROUTES.rxjs.url },
-    { name: APP_ROUTES.rxjs2.name, url: '/' + APP_ROUTES.rxjs2.url },
-    { name: APP_ROUTES.rxjs3.name, url: '/' + APP_ROUTES.rxjs3.url }
-]
--- 4rxjs2/src/app/rxjscommon/rxjs-home/rxjs-home.component.ts
+++ 4rxjs2/src/app/rxjscommon/rxjs-home/rxjs-home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { APP_ROUTES_TO_SIDEBAR } from '../../common/constants/routes/app_routes';
+import { APP_ROUTES } from '../../common/constants/routes/app_routes';
 
 @Component({
   selector: 'app-rxjs-home',
@@ -8,12 +8,16 @@ import { APP_ROUTES_TO_SIDEBAR } from '../../common/constants/routes/app_routes'
 })
 export class RxjsHomeComponent implements OnInit {
 
-  sidebarItems = APP_ROUTES_TO_SIDEBAR;
+  sidebarItems;
 
   constructor() { }
 
   ngOnInit() {
-
+    this.sidebarItems = [
+      { name: APP_ROUTES.rxjs.name, url: '/' + APP_ROUTES.rxjs.url },
+      { name: APP_ROUTES.rxjs2.name, url: '/' + APP_ROUTES.rxjs2.url },
+      { name: APP_ROUTES.rxjs3.name, url: '/' + APP_ROUTES.rxjs3.url }
+    ]
   }
 
 }
